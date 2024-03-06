36abfb94d9c3c33e18c506f3b201e7bfa912d7f3
peleskov <peleskov@yandex.ru>
2024-03-06 16:57:48 +0100

    fix

--- _build/elements/_settings.php
+++ _build/elements/_settings.php
@@ -3,7 +3,7 @@
 return [
     'telegram_bot_api_key' => [
         'xtype' => 'textfield',
-        'value' => '6052061804:AAHJqNyWMwqIE2nHa1xmzZUjwZ045_4ISo8',
+        'value' => '',
         'area' => 'telegramchat_main',
     ],
 ];
